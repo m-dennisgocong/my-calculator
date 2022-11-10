33a16f992781429de481c7bfa19c58bd1d359b7a
Dennis Goc-ong <86005147+m-dennisgocong@users.noreply.github.com>
2022-11-10 21:54:40 +0700

    feat: add header
    contains Clock, toggle theme switch and github logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
-import { useState } from 'react'
+import { useState } from 'react';
+import { FaGithub } from 'react-icons/fa';
+import Clock from './components/Clock';
 import './App.css'
 
 function App() {
+
+  const [theme, setTheme] = useState(true);
+
+  const handleChangeTheme = () => {
+    setTheme(!theme);
+  }
   return (
     <>
+    {/* clock + toggle theme + github logo*/}
+      <header>
+        <Clock/>
+        <div className="switch-theme" onClick={handleChangeTheme}>
+          <div style={!theme ? {transform : 'translateX(100%)'} : null} className='circle' onClick={handleChangeTheme}></div>
+        </div>
+        <FaGithub id='github-logo'/>
+      </header>
 
+    {/* Calculator */}
     </>
   );
 }
