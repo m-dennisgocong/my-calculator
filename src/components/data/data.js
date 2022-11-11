a7b8cc6e95b4b7509cd4eecdfd1663d07229e58e
Dennis Goc-ong <86005147+m-dennisgocong@users.noreply.github.com>
2022-11-11 20:08:10 +0700

    feat: add data contains number, operator and other

---
+++ src/components/data/data.js
@@ -0,0 +1,84 @@
+export const numbers = [
+    {
+        "number" : 0,
+        "name" : "zero", 
+    },
+    {
+        "number" : 1,
+        "name" : "one", 
+    },
+    {
+        "number" : 2,
+        "name" : "two", 
+    },
+    {
+        "number" : 3,
+        "name" : "three", 
+    },
+    {
+        "number" : 4,
+        "name" : "four", 
+    },
+    {
+        "number" : 5,
+        "name" : "five", 
+    },
+    {
+        "number" : 6,
+        "name" : "six", 
+    },
+    {
+        "number" : 7,
+        "name" : "seven", 
+    },
+    {
+        "number" : 8,
+        "name" : "eight", 
+    },
+    {
+        "number" : 9,
+        "name" : "nine", 
+    }
+]; 
+
+export const operators = [
+    {
+        "operator" : '+',
+        "name" : "add"
+    },
+    {
+        "operator" : "-",
+        "name" : "subtract"
+    },
+    {
+        "operator" : '÷',
+        "name" : "divide" 
+    },
+    {
+        "operator" : 'x',
+        "name" : "multiply"
+    }
+];
+
+export const otherFunction = [
+    {
+        "symbol" : "AC",
+        "name" : "allClear"
+    },
+    {
+        "symbol" : "CE",
+        "name" : "clearEntry"
+    },
+    {
+        "symbol" : ".",
+        "name" : "decimal"
+    },
+    {
+        "symbol" : "=",
+        "name" : "equal"
+    },
+    {
+        "symbol" : "±",
+        "name" : "toggleSign"
+    }
+];
