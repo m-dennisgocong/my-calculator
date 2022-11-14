e3cc5bb7d358b23754c5b39ebe6149a22efc3b06
Dennis Goc-ong <86005147+m-dennisgocong@users.noreply.github.com>
2022-11-14 19:22:03 +0700

    feat: add Screen component with id display

--- src/components/Calculator/Calculator.jsx
+++ src/components/Calculator/Calculator.jsx
@@ -1,9 +1,17 @@
 import { useState } from "react";
 import Buttons from "./Buttons";
+
+const Screen = () => {
+    return <div id="display"></div>
+}
+
 const Calculator = () => {
     
     return(
+    <>
+        <Screen/>
         <Buttons/>
+    </>
     );
 }
 export default Calculator;
