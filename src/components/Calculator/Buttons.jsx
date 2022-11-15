814cb0b7902449ee8cfc69f93d8604117d47a592
Dennis Goc-ong <86005147+m-dennisgocong@users.noreply.github.com>
2022-11-15 21:00:46 +0700

    refactor: add buttons function
    Add the buttons function for number, operator, AC(All Clear) and equal from Calculator Component.
    
    close #7, close #8, close #11

--- src/components/Calculator/Buttons.jsx
+++ src/components/Calculator/Buttons.jsx
@@ -1,9 +1,23 @@
 import { buttons } from "../data/data";
 
-const Buttons = () => {
+const Buttons = ({handleNumbers, handleOperators, allClear, handleEvaluate}) => {
+
     return(
         <>
-        {buttons.map((buttons, index) => <button key={index} id={buttons.name} value={buttons.symbol} onClick={()=> console.log(buttons.name)}>{buttons.symbol}</button>)}
+            {buttons.map((buttons, index) => {
+                switch(buttons.type){
+                    case 'number': 
+                        return <button key={index} id={buttons.name} value={buttons.symbol} onClick={(e) => handleNumbers(e)}>{buttons.symbol}</button>
+                    case 'operator':
+                        return <button key={index} id={buttons.name} value={buttons.symbol} onClick={(e) => handleOperators(e)}>{buttons.symbol}</button>
+                    case 'remove': // clear all
+                        return <button key={index} id={buttons.name} value={buttons.symbol} onClick={allClear}>{buttons.symbol}</button>
+                    case 'equation':
+                        return <button key={index} id={buttons.name} value={buttons.symbol} onClick={handleEvaluate}>{buttons.symbol}</button>
+                    default:
+                        return <button key={index} id={buttons.name} value={buttons.symbol} onClick={()=> console.log(buttons.name+ 'under construction')}>{buttons.symbol}</button>
+                }
+            })}
         </>
     );
 }
