1bb1006082ddffeb5f96985d9968d79b74df380b
Dennis Goc-ong <86005147+m-dennisgocong@users.noreply.github.com>
2022-11-27 04:43:23 +0700

    feat: import ThemeContext and useContext

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,35 @@
-import { useState } from 'react';
+import { useState, useContext } from 'react';
 import { FaGithub } from 'react-icons/fa';
 import Clock from './components/Clock';
 import Calculator from './components/Calculator/Calculator';
+import {ThemeContext} from './Theme';
 import './App.css'
 
 function App() {
 
-  const [theme, setTheme] = useState(true);
+  const {theme, toggleTheme} = useContext(ThemeContext);
+  const [slide, setSlide] = useState(theme === 'dark-theme' ? true:false);
 
   const handleChangeTheme = () => {
-    setTheme(!theme);
+    setSlide(!slide);
+    toggleTheme()
   }
+
   return (
-    <>
+    <div className={`App ${theme}`}>
     {/* clock + toggle theme + github logo*/}
       <header>
         <Clock/>
         <div className="switch-theme" onClick={handleChangeTheme}>
-          <div style={!theme ? {transform : 'translateX(100%)'} : null} className='circle' onClick={handleChangeTheme}></div>
+          <div style={!slide ? {transform : 'translateX(95%)'} : null} className='circle' onClick={handleChangeTheme}></div>
         </div>
         <FaGithub id='github-logo'/>
       </header>
 
     {/* Calculator */}
-    <Calculator />
+      <Calculator />
 
-    </>
+    </div>
   );
 }
 
