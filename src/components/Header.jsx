7acdd673c8266b5a3caa9eb32019563aa97eaad9
Dennis Goc-ong <86005147+m-dennisgocong@users.noreply.github.com>
2022-11-28 04:28:53 +0700

    feat: add a tag for github repo link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -20,7 +20,7 @@ const Header = () => {
             <div className="switch-theme" onClick={handleChangeTheme}>
                 <div style={!slide ? {transform : 'translateX(95%)'} : null} className='circle' onClick={handleChangeTheme}></div>
             </div>
-            <FaGithub id='github-logo'/>
+            <a href="#" id='github-logo'><FaGithub /></a>
         </header>
     );
 }
